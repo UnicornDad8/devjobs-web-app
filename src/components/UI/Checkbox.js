c713c24995a42f7afade0592df7c271d86ee4fbb
UnicornDad8 <unicorndad8@gmail.com>
2024-07-29 11:34:27 -0400

    styled checkbox UI component

--- src/components/UI/Checkbox.js
+++ src/components/UI/Checkbox.js
@@ -5,15 +5,21 @@ const Checkbox = forwardRef((props, ref) => {
   const text = useResponsiveText(props.text, props.responsiveText);
 
   return (
-    <label htmlFor={props.id}>
+    <label
+      htmlFor={props.id}
+      className={`"relative flex items-center gap-[1.5rem] font-semibold cursor-pointer capitalize search-bar-items checkbox-label shrink-0 hover:bg-[#d5d8f7]" ${
+        props.hideOnMobile ? "hide-on-mobile" : ""
+      }`}
+    >
       <input
         onChange={props.onChange}
         id={props.id}
         type="checkbox"
         ref={ref}
         checked={props.checked}
+        className="absolute w-0 h-0 opacity-0 checkbox-default-input"
       />
-      <span></span>
+      <span className="shrink-0 w-[2.4rem] h-[2.4rem] bg-checkbox rounded-[0.3rem] relative duration-200 ease-linear hover:bg-[#d5d8f7] checkbox-custom-input"></span>
       {text}
     </label>
   );
