3ebf75dc6e3a138f5406c8cc14aa3204f3bcd9e4
UnicornDad8 <unicorndad8@gmail.com>
2024-07-29 09:16:55 -0400

    Added some base styles and styled background svg

--- src/components/Header/BackgroundSVG.js
+++ src/components/Header/BackgroundSVG.js
@@ -1,5 +1,7 @@
 const BackgroundSVG = () => {
-  return <div></div>;
+  return (
+    <div className="back-svg-image absolute top-0 left-0 w-full h-[16rem] bg-no-repeat bg-cover md:rounded-bl-[10rem] rounded-bl-0"></div>
+  );
 };
 
 export default BackgroundSVG;
